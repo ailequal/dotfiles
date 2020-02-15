c73fa3226bcbdbd11f3d738e3f0dca199a929df3
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-15 19:01:32 +0100

    Added laravel-mix-init

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -10,26 +10,51 @@ alias ciao-mondo="echo hello-world"
 
 # initialize a github repo with a custom name (if specified)
 function repo-hub() {
-				# generating files
-				mkdir $1 && cd $1
-				nvim README.md
+		# generating files
+		mkdir $1 && cd $1
+		vim README.md
 
-				# generating github repo
-				git init
-				git add .
-				git commit -m 'First commit'
-				hub create
-				git push --set-upstream origin master
+		# generating github repo
+		git init
+		git add .
+		git commit -m 'First commit'
+		hub create
+		git push --set-upstream origin master
 }
 
 # initialize a git repo with a custom name (if specified)
 function repo-local() {
-				# generating files
-				mkdir $1 && cd $1
-				nvim README.md
+		# generating files
+		mkdir $1 && cd $1
+		vim README.md
 
-				# generating git repo
-				git init
-				git add .
-				git commit -m 'First commit'
+		# generating git repo
+		git init
+		git add .
+		git commit -m 'First commit'
+}
+
+# install laravel mix in the current folder (using npm)
+function laravel-mix-init() {
+		# generating files
+		npm init -y
+		npm install laravel-mix --save-dev
+		cp node_modules/laravel-mix/setup/webpack.mix.js ./
+		mkdir src && touch src/app.{js,scss}
+		node_modules/.bin/webpack --config=node_modules/laravel-mix/setup/webpack.config.js
+
+		# add custom scripts to package.json
+		cat ~/.copy/package.json.copy | pbcopy
+		vim package.json
+
+		# for handling different environments
+		npm install cross-env --save-dev
+
+		# compile files once
+		npm run dev
+
+		# add custom .gitignore
+		touch .gitignore
+		cat ~/.copy/.gitignore.copy | pbcopy
+		vim .gitignore
 }
