0935ad1ad8c8927305e132178febea2f5f22dd99
Elia Pari <37016865+ailequal@users.noreply.github.com>
2023-11-08 07:26:03 +0100

    updated git-backup script

--- macos/git-backup.sh
+++ macos/git-backup.sh
@@ -4,6 +4,14 @@
 # git-backup #
 ##############
 
+# Inside the "repos.txt" file, set the repositories names, one per line.
+# The target folder is always generated in the current directory where the script is executed.
+# Inside the new folder, each repository will be stored inside a tar.gz archive.
+# Each repository is cloned as a mirror, so you can push it to a new remote.
+# To restore a repository, just untar the archive and git clone its mirror.
+# Example for executing the script with a few arguments:
+# ~/git-backup.sh -u john.doe -r ~/repos.txt
+
 
 
 echo ""
@@ -18,15 +26,17 @@ repos="repos.txt"
 target="git-backup-$date"
 count=1
 
-while getopts d:u:r:t: FLAG
+while getopts "d:u:r:t:" FLAG
 do
   case "${FLAG}" in
-    d) domain=${OPTARG};;
-    u) username=${OPTARG};;
-    r) repos=${OPTARG};;
-    t) target=${OPTARG};;
+    d) domain="${OPTARG}" ;;
+    u) username="${OPTARG}" ;;
+    r) repos="${OPTARG}" ;;
+    t) target="${OPTARG}" ;;
   esac
 done
+
+target=$(echo "$target" | tr -d '[:space:]')
 
 if [ -z "$domain" ]; then
   echo "\$domain cannot be empty (-d)."
@@ -54,14 +64,14 @@ if [ -z "$target" ]; then
 fi
 
 if [ -d "$target" ]; then
-  echo "\$target already exist, set a new one (-r)."
+  echo "\$target already exists, set a new one (-t)."
   exit 0
 fi
 
-echo "Domain:   $domain";
-echo "Username: $username";
-echo "Repos:    $repos";
-echo "Target:   $target";
+echo "Domain:   $domain"
+echo "Username: $username"
+echo "Repos:    $repos"
+echo "Target:   $target"
 echo ""
 
 echo "Double check your configuration above."
@@ -74,20 +84,24 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "##############"
   echo ""
 
-  mkdir $target
+  mkdir "$target"
 
-  while read repo; do
-    echo "$count: $repo"
+  while IFS= read -r repo; do
+    repo=$(echo "$repo" | tr -d '[:space:]')
 
-    ssh_repo=$domain:$username/$repo.git
-    git clone --mirror $ssh_repo ./$target/$repo
-    tar -zcvf ./$target/$repo.bak.tar.gz -C ./$target $repo
-    rm -rf ./$target/$repo
-    count=$(($count+1))
+    if [ -n "$repo" ]; then
+      echo "$count: $repo"
 
-    echo "##########"
-    echo ""
-  done < $repos
+      ssh_repo="$domain:$username/$repo.git"
+      git clone --mirror "$ssh_repo" "./$target/$repo"
+      tar -zcvf "./$target/$repo.bak.tar.gz" -C "./$target" "$repo"
+      rm -rf "./$target/$repo"
+      count=$((count+1))
+
+      echo "##########"
+      echo ""
+    fi
+  done < "$repos"
 
   echo "# end #"
   echo "#######"
